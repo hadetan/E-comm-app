ad00ae2d8aace555d989e3a4d3f0c938676d0503
Aquib <83490342+hadetan@users.noreply.github.com>
2024-05-28 08:59:06 +0000

    forgot password and mail implimentation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,6 +4,8 @@ import asyncHandler from "../utils/asynchHandler.js"
 import customError from "../utils/customError.js"
 import User from "../models/user.schema.js"
 import userSchema from "../models/user.schema.js"
+import mailHelper from "../utils/mailHelper.js"
+// import cookieParser from "cookie-parser"
 
 export const cookieOptions = {
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
@@ -142,5 +144,62 @@ export const forgotPassword = asyncHandler( async(req, res) => {
 
     const message = `Your password reset token is as follows /n/n ${resetPasswordUrl} /n/n if this was not requested by you, please ignore this mail.`;
 
-    
+    try {
+        const options = {
+            email: user.email,
+            subject: "Email to reset your password",
+            message
+        }
+
+        await mailHelper({options})
+    } catch (error) {
+        user.forgotPasswordExpiry = undefined;
+        user.forgotPasswordToken = undefined;
+
+        await user.save({validateBeforeSave: false});
+
+        throw new customError(error.message || "Email could not be sent", 500);
+    }
 });
+
+export const resetPassword = asyncHandler( async(req, res) => {
+    const {token: resetToken} = req.params;
+    const {password, confirmPassword} = req.body;
+
+    if (
+        !password ||
+        !confirmPassword
+    ) {
+        throw new customError("Password and Confirm password is required", 400);
+    }
+
+    if (password !== confirmPassword) {
+        throw new customError("Password did not match with confirm password")
+    }
+
+    const resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex")
+
+    const user = await User.findOne({
+        forgotPasswordToken: resetPasswordToken,
+        forgotPasswordExpiry: { $gt : Date.now() }
+    })
+
+    user.password = password;
+    user.forgotPasswordToken = undefined;
+    user.forgotPasswordExpiry = undefined;
+
+    await user.save();
+
+    // optional
+    const token = user.getJWTtoken;
+    res.cookie("token", token, cookieOptions);
+
+    res.status(200).json({
+        success: true,
+        user,
+        token
+    })
+})
